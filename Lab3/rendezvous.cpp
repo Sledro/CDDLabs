d0a576fc3952229a49308cf888a5be4137e8f590
sledro <dan@sledro.com>
2017-11-06 10:14:21 +0000

    Next lab added

--- Lab3/rendezvous.cpp
+++ Lab3/rendezvous.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <thread>
 
-void taskOne(std::shared_ptr<Semaphore> theSemaphore1, std::shared_ptr<Semaphore> theSemaphore2){
+void taskOne(std::shared_ptr<Semaphore> theSemaphore){
   std::cout << "Queue task A."<<std::endl;
   theSemaphore1->Signal();
   std::cout << "Run A."<<std::endl;
@@ -14,21 +14,21 @@ void taskOne(std::shared_ptr<Semaphore> theSemaphore1, std::shared_ptr<Semaphore
   std::cout << "Thread A Complete."<<std::endl;
  
 }
-void taskTwo(std::shared_ptr<Semaphore> theSemaphore1, std::shared_ptr<Semaphore> theSemaphore2){
+void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
   std::cout << "Queue task B."<<std::endl;
-  theSemaphore2->Signal();
+  theSemaphore1->Signal();
   std::cout << "Run B."<<std::endl;
-  theSemaphore1->Wait();
+  theSemaphore2->Wait();
   std::cout << "Thread B Complete."<<std::endl;
   
 }
 
 int main(void){
   std::thread threadOne, threadTwo;
-  std::shared_ptr<Semaphore> theSemaphore1( new Semaphore);
-  std::shared_ptr<Semaphore> theSemaphore2( new Semaphore);
-  threadOne=std::thread(taskTwo,theSemaphore1,theSemaphore2);
-  threadTwo=std::thread(taskOne,theSemaphore1,theSemaphore2);
+  std::shared_ptr<Semaphore> sem1( new Semaphore);
+  std::shared_ptr<Semaphore> sem2( new Semaphore);
+  threadOne=std::thread(taskTwo,sem1,sem2);
+  threadTwo=std::thread(taskOne,sem1,sem2);
   std::cout << "Main method started..\n";
   threadOne.join();
   threadTwo.join();
