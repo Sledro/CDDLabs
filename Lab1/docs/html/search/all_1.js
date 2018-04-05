0c8a812ebb64ba63c782109650234fe4f5bca742
sledro <dan@sledro.com>
2018-04-05 17:18:52 +0100

    Doc commends improved. Image and descriptions. Mainpage added to Docs.

---
+++ Lab1/docs/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main',['main',['../signal_8cpp.html#a840291bc02cba5474a4cb46a9b9566fe',1,'signal.cpp']]],
+  ['mainpage_2emd',['MAINPAGE.md',['../_m_a_i_n_p_a_g_e_8md.html',1,'']]]
+];
