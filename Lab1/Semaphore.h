3aafc1ea6c800e0de96a9ea9f4769199dee531ad
sledro <dan@sledro.com>
2018-04-05 15:52:16 +0100

    Removed unused function defs

--- Lab1/Semaphore.h
+++ Lab1/Semaphore.h
@@ -1,6 +1,5 @@
 #include <mutex>
 #include <condition_variable>
-#include <chrono>
 
 class Semaphore
 {
@@ -10,11 +9,7 @@ private:
     std::condition_variable m_condition;
 
 public:
-    Semaphore(unsigned int uiCount=0)
-          : m_uiCount(uiCount) { };
     void Wait();
-    template< typename R,typename P >
-    bool Wait(const std::chrono::duration<R,P>& crRelTime);
     void Signal();
 
 };
