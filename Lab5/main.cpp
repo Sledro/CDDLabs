b8f1d82ea7d0279c21eb3bce58cfb44448e501b7
sledro <dan@sledro.com>
2018-04-06 17:48:14 +0100

    Now stops producing after 20 chars

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -10,23 +10,30 @@
 #include <mutex>
 #include <chrono>
 
+//Number of charater that for the producer method to produce
+int NUM_OF_CHARS_TO_PRODUCE = 20;
+
 /*! The producer generates random characters from ‘a’ to ‘z’ at random intervals (between 0 and 1 second in length). It adds these to a thread safe buffer 
     that has a finite holding capacity of N characters. It generates a preset number of characters (determined at runtime) and when it has finished it add 
     an ‘X’ character to the buffer and exits.
     \param none` 
-    \return void.
+    \return void.¸
 */ 
 void makeProducers(std::shared_ptr<SafeBuffer> safeBuff){
 
     char letters[] = "abcdefghijklmnopqrstuvwxyz";
     char randomChar = letters[rand() % 26];
+    int counter=0;
 
     while(randomChar!='X'){
         std::this_thread::sleep_for(std::chrono::milliseconds(std::rand()%1000));
         //pick a random character between 0 and the number of characters stored in the array
         randomChar = letters[rand() % 26];
         safeBuff->add(randomChar);
-        std::cout << randomChar << std::endl;
+        if(counter==NUM_OF_CHARS_TO_PRODUCE)
+            randomChar='X';
+        std::cout << randomChar << " was added to the safe buffer" std::endl;
+        counter++;
     }
 }
 
